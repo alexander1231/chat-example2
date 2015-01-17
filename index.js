4f21dbcaf7c6699ec80cbf9e5f18403f769f10e5
Hanshavin <hansevangelista@gmail.com>
2015-01-17 03:31:39 -0500

    add brodcasting

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.get('/tucutin.mp3', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+      // io.emit('chat message', msg);
+      socket.broadcast.emit('chat message', msg);
   });
 });
 
